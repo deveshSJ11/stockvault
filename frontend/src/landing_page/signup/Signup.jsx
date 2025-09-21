903e81d7f2aeb5fa83f462af53c4467d653e1005
Devesh <deveshjaiswal1212@gmail.com>
2025-09-21 22:39:54 +0530

    Added routing setup with Navbar, Footer, and Notfound page fixes

---
+++ frontend/src/landing_page/signup/Signup.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Signup() {
+  return <div>Signup Page Works!</div>;
+}
+
+export default Signup;
---
+++ frontend/src/landing_page/support/SupportPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SupportPage() {
+  return (
+    <div>
+      <h1>Support Page</h1>
+      <p>This is where support information goes.</p>
+    </div>
+  );
+}
+
+export default SupportPage;
