78125be78f31cb61ed351b41521d6f623911234f
Devesh <deveshjaiswal1212@gmail.com>
2025-09-25 22:55:20 +0530

    Initial commit for frontendUI

---
+++ src/landing_page/about/AboutPage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+function AboutPage() {
+  return (<div>Signup Page</div>);
+}
+
+export default AboutPage;
+
+import Hero from "./Hero";
+import Team from "./Team";
+
+function PricingPage() {
+  return (
+    <>
+      <Hero />
+      <Team />
+    </>
+  );
+}
